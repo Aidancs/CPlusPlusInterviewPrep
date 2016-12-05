b1db29c91ede70b130825a1a0a30b66b1df3cb19
Aidancs <aidancs316@gmail.com>
2016-12-04 20:32:00 -0600

    1st commit
    First commit, initial c++ file template

---
+++ HashTableImp/HashTableImp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HashTableImp
+//
+//  Created by Aidan Sullivan on 12/4/16.
+//  Copyright © 2016 Aidan Sullivan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
